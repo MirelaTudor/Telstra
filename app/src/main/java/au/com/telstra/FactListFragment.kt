aa367431171dad440027f564ec77aea4565c9cda
mirela.tudor <M1r3l@Lalu>
2020-05-07 20:57:54 +0300

    Android project structure

---
+++ app/src/main/java/au/com/telstra/FactListFragment.kt
@@ -0,0 +1,25 @@
+package au.com.telstra
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FactListFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fact_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
